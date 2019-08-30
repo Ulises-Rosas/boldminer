66a79f4548ebe3f9c46b3a72cb58c468b1480f7f
Ulises-Rosas <ulisesfrosasp@gmail.com>
2019-08-30 15:16:05 -0500

    first commit

---
+++ R/SpecimenData.R
@@ -0,0 +1,65 @@
+#' SpecimenData
+#'
+#' This function let us mine associated metadata
+#' from any specimen according to query arguments.
+#' It takes very specific queries and
+#' use them as headers for BOLD API host
+#' for downloading metadata. More than one query
+#' can be specified by using "|".
+#'
+#' @param taxon Species or taxa to look for
+#' @param ids IDs from specimen records. e.g. ANGBF12704-15
+#' @param bin barcode-based cluster ID. e.g. BOLD:AAA4689
+#' @param container container name. e.g. FIPP
+#' @param institution institution names. e.g. Smithsonian Institution
+#' @param reseachers names of identifiers or specimen collectors
+#' @param geo name of geographical area to look for.
+#' @param ... others query name or column name that can be present in metadata
+#'
+#' @return metadata information
+#' @export
+#'
+
+
+# library(ape)
+# library(data.table)
+# library(RCurl)
+
+SpecimenData <- function(taxon, ids, bin, container,
+                         institutions, researchers, geo, ...){
+        input <- data.frame(
+                names = names(as.list(environment())),
+                args = sapply(as.list(environment()), paste),
+                stringsAsFactors = F
+        )
+
+        #text <- RCurl::getURL(
+        URLtxt <- paste(if(list(...)[1] == "only"){
+                "http://www.boldsystems.org/index.php/API_Public/sequence?"}
+                else{if(list(...)[1] == "combined"){
+                        "http://www.boldsystems.org/index.php/API_Public/combined?"}
+                        else{
+                                "http://www.boldsystems.org/index.php/API_Public/specimen?"}
+                },
+                paste(
+                        paste(input[!(input$args == ""),]$names,
+                              "=",
+                              sapply(input[!(input$args == ""),]$args, function(x){
+                                      if(length(strsplit(x, " ")[[1]]) > 1){
+                                              paste(gsub(" ", "%20", x), "&", sep = "")
+                                      }else{paste(x, "&", sep = "")}}
+                              ),
+                              sep = ""),
+                        collapse = ""),
+                "format=tsv",
+                sep = "")
+        text <- RCurl::getURL(URLtxt)
+
+        if(list(...)[1] == "only")
+          return(ape::read.FASTA(textConnection(text)))
+
+        if(text == "")
+          return(text)
+
+        data.table::fread(text,quote = "")
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+boldminer
+=========
+
+<!-- badges: start -->
+<!-- badges: end -->
+boldminer allows to mine metadata from BOLD database by using its API service and presents this up for downstream procedures. Furthermore, boldminer also make quick assessment of quality of species barcodes.
+
+Installation
+------------
+
+You can install the released version of boldminer from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("boldminer")
+```
+
+Example
+-------
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(boldminer)
+## basic example code
+```
