7d4d988a7f318916150e676c6bebb646906070f5
Ulises-Rosas <ulisesfrosasp@gmail.com>
2019-10-23 14:16:55 -0500

    adding leafletPlot

--- R/SpecimenData.R
+++ R/SpecimenData.R
@@ -73,7 +73,7 @@ SpecimenData <- function(taxon, ids, bin, container, institutions, researchers, 
 #' short utility to summarize data frame
 #'
 #' @param df specimen data from \code{\link{SpecimenData}} function
-#' @param columns columns to summarize on
+#' @param cols columns to summarize on
 #'
 #' @return summarized data frame
 #' @export
@@ -111,3 +111,88 @@ sumSData <- function(df, cols){
 
   return(out)
 }
+
+#' leafletPlot
+#'
+#' Interactive map of barcodes, when geographic information is available
+#'
+#' @param df specimen data from \code{\link{SpecimenData}} function
+#' @param col column used as label
+#'
+#' @return leaflet map
+#' @export
+#' @importFrom data.table .SD
+#' @importFrom magrittr %>%
+#' @importFrom stats complete.cases
+#' @importFrom leaflet leaflet addProviderTiles fitBounds addCircleMarkers providerTileOptions
+#' @examples
+#' \dontrun{
+#' specimendata <- boldminer::SpecimenData(taxon = "Elasmobranchii", geo = "Peru")
+#' boldminer::leafletPlot(df = specimendata, col = "species_name")
+#' }
+
+leafletPlot <- function(df, col = "species_name"){
+  # df = specimendata
+  # col = "species_name"
+
+  sumSData(
+    df   = df,
+    cols = c(col, "lat", "lon")
+    ) -> wholedf
+
+  toPlot <- wholedf[complete.cases(wholedf),]
+
+  if( !nrow(toPlot) ){
+    warning("Any geographic information in specimen data")
+    return(NULL)
+  }
+
+  ifelse(
+    col == "species_name",
+    "<i><strong>%s</strong><br/></i>%g specimens",
+    "<strong>%s</strong><br/>%g specimens"
+    ) -> fmt
+
+  as.list(
+    sprintf(
+      fmt,
+      unlist(
+        toPlot[,
+               col,
+               with = F
+               ][,
+                 lapply( .SD, function(y) ifelse(y == "", "NA", as.character(y)) )
+                 ]
+        ),
+      toPlot$n
+      )
+    ) -> labels
+
+  leaflet::leaflet(toPlot) %>%
+    leaflet::addProviderTiles(
+      leaflet::providers$Esri.WorldImagery,
+      options = leaflet::providerTileOptions(minZoom = 2, maxZoom = 17)
+    ) %>%
+    leaflet::fitBounds(
+      min(toPlot$lon),
+      min(toPlot$lat),
+      max(toPlot$lon),
+      max(toPlot$lat)
+    ) %>%
+    leaflet::addCircleMarkers(
+      ~lon,
+      ~lat,
+      weight = 15,
+      radius = ~(n*100/sum(n)),
+      clusterOptions = leaflet::markerClusterOptions(),
+      popup = labels,
+      stroke = T,
+      fillOpacity = 1,
+      opacity = 0.8,
+      color = "#ff0000"
+    ) -> q
+
+  return(q)
+}
+
+
